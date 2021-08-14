b010ef161167c2613ef069eda160268bf515fd16
Belle Duran <bduran04@gmail.com>
2021-08-14 18:02:48 -0500

    updated the technical skills format

--- src/About/index.js
+++ src/About/index.js
@@ -1,4 +1,4 @@
-import { Avatar, Button, Card, CardContent, CardMedia, Container, Grid, makeStyles, Typography } from '@material-ui/core';
+import { Button, Card, CardContent, Container, Divider, Grid, List, ListItem, ListItemText, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 import pic from "../images/BD_image.jpg";
 import GetAppRoundedIcon from '@material-ui/icons/GetAppRounded';
@@ -14,7 +14,7 @@ const useStyles = makeStyles({
      text: {
           display: "grid",
 
-     }
+     },
 });
 
 const About = () => {
@@ -24,7 +24,7 @@ const About = () => {
           link.download = resume;
           link.href = resume;
           link.click();
-        };
+     };
 
      return (
           <Container style={{ height: '100%' }}>
@@ -37,13 +37,13 @@ const About = () => {
                     alignItems="center"
                >
                     <Grid item xl={6} md={4} sm={12} xs={12} >
-                         <img style={{ height: 400 }} src={pic} />
+                         <img style={{ height: 400 }} src={pic} alt="Belle2" />
                     </Grid>
                     <Grid item xl={6} md={6} sm={12} xs={12}>
                          <p className="about-text">
                               I received a Certificate in Full Stack Web Development from Southern Methodist University where I learned best practices for writing clean code and designing mobile-friendly websites and applications.
                               <br />
-                               I've also completed the relevant courses: Numerical Analysis, Abstract Algebra, Matrix Theory, and Vector Calculus towards a BA in Mathematics from the University of North Texas. 
+                              I've also completed the relevant courses: Numerical Analysis, Abstract Algebra, Matrix Theory, and Vector Calculus towards a BA in Mathematics from the University of North Texas.
                               <br />
                               <br />
                               On my down time, I enjoy spoiling my cats, Kitsune and Mittens, learning new recipes, and tending my garden.
@@ -61,30 +61,63 @@ const About = () => {
                     <Grid item xs={12} sm={6} y={10}>
                          <Card >
                               <CardContent>
-                                   <Typography>
+                                   <Typography variant="h6">
                                         Technical Skills
                                    </Typography>
-                                   <Typography>
-                                        <ul align="left">
-                                             <li>
-                                                  Programming Languages: React.js, Next.js, JavaScript, Express.js, Node.js, HTML, CSS, SQL, NoSQL
-                                             </li>
-                                             <li>
-                                                  Database Management: Mysql, MongoDB
-                                             </li>
-                                             <li>
-                                                  Library/Frameworks: Material-UI, Bootstrap, Geist-UI, Materialize
-                                             </li>
-                                             <li>
-                                                  Other Technologies: Vercel, Git, Handlebars, Heroku, IndexedDB, API
-                                             </li>
-                                        </ul>
-                                   </Typography>
-                                   <Button 
-                                   variant="contained" 
-                                   color="secondary"
-                                   onClick={onDownload}>
-                                   <GetAppRoundedIcon />
+                                   <List align="left">
+                                        <ListItem>
+                                             <ListItemText primary="Programming Languages:"
+                                                  secondary={
+                                                       <React.Fragment>
+                                                            <Typography variant="body2">
+                                                                 React.js, Next.js, JavaScript, Express.js, Node.js, HTML, CSS, SQL, NoSQL
+                                                            </Typography>
+                                                       </React.Fragment>
+                                                  }>
+                                             </ListItemText>
+                                        </ListItem>
+                                        <Divider />
+                                        <ListItem>
+                                             <ListItemText primary="Database Management:"
+                                                  secondary={
+                                                       <React.Fragment>
+                                                            <Typography variant="body2">
+                                                                 Mysql, MongoDB
+                                                            </Typography>
+                                                       </React.Fragment>
+                                                  }>
+                                             </ListItemText>
+                                        </ListItem>
+                                        <Divider />
+                                        <ListItem>
+                                             <ListItemText primary="Library/Frameworks:"
+                                                  secondary={
+                                                       <React.Fragment>
+                                                            <Typography variant="body2">
+                                                                 Material-UI, Bootstrap, Geist-UI, Materialize
+                                                            </Typography>
+                                                       </React.Fragment>
+                                                  }>
+                                             </ListItemText>
+                                        </ListItem>
+                                        <Divider />
+                                        <ListItem>
+                                             <ListItemText primary="Other Technologies:"
+                                                  secondary={
+                                                       <React.Fragment>
+                                                            <Typography variant="body2">
+                                                                 Vercel, Git, Handlebars, Heroku, IndexedDB
+                                                            </Typography>
+                                                       </React.Fragment>
+                                                  }>
+                                             </ListItemText>
+                                        </ListItem>
+                                   </List>
+                                   <Button
+                                        variant="contained"
+                                        color="secondary"
+                                        onClick={onDownload}>
+                                        <GetAppRoundedIcon />
                                         Download Resume
                                    </Button>
                               </CardContent>
